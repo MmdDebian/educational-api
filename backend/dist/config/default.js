c15d63abb4d30c990d7f5946640ad6b22df89630
mohamad <mohamadmirzaei.ir@gmail.com>
2022-08-09 23:51:50 +0430

    delete file

--- backend/dist/config/default.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    http_port: 3001,
-    jwt_key: 'wefqefefqwegfbwefby4rib41679238yrhfejchgfqwjefhkwjhf'
-};
